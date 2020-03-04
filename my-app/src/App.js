adefcb100241c7b930b857e04acb6c0c4703b40a
RakibulIslam1161062 <bsse0903@iit.du.ac.bd>
2020-03-05 01:19:56 +0600

    show as list items

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 
 import './App.css';
+import './Ninja';
+import Ninja from './Ninja';
 
 class App extends Component {
+  state = {
+    ninjas : [
+      { name:'Rakib', age:'22',id:"1"},
+      { name:'Raks', age:'22',id:"2"}
+    ]
+  }
   render() {
     return (
       <div className="App">
         <p>First app in React </p>
+        <Ninja ninjas={this.state.ninjas} />
       </div>
     );
   }
