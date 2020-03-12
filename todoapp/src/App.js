6ba45dfb4c4ce7c11ed77f3ca602d462c62c0396
rakibsxyz <bsse0903@iit.du.ac.bd>
2020-03-12 13:59:14 +0600

    bug fixed

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -10,8 +10,8 @@ class App extends Component {
   }
   render() {
     return(
-    <div className="App">
-     <h1 className='center blue-Text'>My Todo's</h1>
+    <div className="todo-app container">
+     <h1 className='center blue-text'>My Todo's</h1>
      <Todos todos={this.state.todos} />
     </div>
   );
