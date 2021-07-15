9391d92039bb84231024efba5c9249010c27f395
RakibulIslam1161062 <bsse0903@iit.du.ac.bd>
2021-07-15 18:05:36 +0600

    unsplash project added

--- boxshadowgenerator/src/Components/Unsplash.tsx
+++ boxshadowgenerator/src/Components/Unsplash.tsx
@@ -1,18 +1,34 @@
-import React,{useState}from 'react'
+import React, { useState } from 'react'
 import Search from './Search'
 
 function Unsplash() {
     const [images, setImages] = useState<any>([])
+    const [imageResults, setImageResults] = useState<any>([])
 
-    const onSearchSend = (searchText:any) =>{
+    const onSearchSend = (searchText: any) => {
         console.log(searchText)
-        fetch(`https://api.unsplash.com/search/photos&query=${searchText}/?client_id=EHCkrxohpaxWbkZy_acgXaJv-PLUQmA5wp2tBBGCjOc`)
-        // EHCkrxohpaxWbkZy_acgXaJv-PLUQmA5wp2tBBGCjOc
+        fetch(`https://api.unsplash.com/search/photos?client_id=EHCkrxohpaxWbkZy_acgXaJv-PLUQmA5wp2tBBGCjOc&query=${searchText}&orientation=squarish`)
+            // EHCkrxohpaxWbkZy_acgXaJv-PLUQmA5wp2tBBGCjOc
+            .then(res => res.json())
+            .then(res => {
+                console.log(res)
+                setImageResults(res.results)
+                // console.log(imageResults[0].urls)
+            })
     }
 
     return (
-        <div style={{width:"100%"}} >
-            <Search onSendClick={onSearchSend}/>   
+        <div style={{ width: "100%" }} >
+            <Search onSendClick={onSearchSend} />
+            <div className="gallary" >
+                {imageResults && imageResults.map((image: any) => {
+                    return <div> <img className="image" key={image.id} src={image.urls.regular} />
+                    </div>
+
+                })
+                }
+            </div>
+
         </div>
     )
 }
