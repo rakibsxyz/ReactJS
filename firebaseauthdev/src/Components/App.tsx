3835e6170d33280652db0c62a358270d78d7cdba
rakibsxyz <bsse0903@iit.du.ac.bd>
2021-08-02 10:52:21 +0600

    firebase project added

---
+++ firebaseauthdev/src/Components/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Signup from './Signup';
+import {Container} from 'react-bootstrap'
+
+
+function App() {
+  return  (
+    <Container>
+    <Signup/>
+   </Container>
+  );
+}
+
+export default App;
