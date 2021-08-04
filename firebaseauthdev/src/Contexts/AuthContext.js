3dee529993221ca434fba96794cd59aceed089e8
RakibulIslam1161062 <bsse0903@iit.du.ac.bd>
2021-08-04 18:02:45 +0600

    authentication done

--- firebaseauthdev/src/Contexts/AuthContext.js
+++ firebaseauthdev/src/Contexts/AuthContext.js
@@ -30,6 +30,14 @@ export function AuthProvider({ children }) {
         return auth.sendPasswordResetEmail(email)
     }
 
+    function updateEmail(email) {
+        return currentUser.updateEmail(email)
+    }
+
+    function updatePassword(password) {
+        return currentUser.updatePassword(password)
+    }
+
     useEffect(() => {
         const unsubscribe = auth.onAuthStateChanged(user => {
             setCurrentUser(user)
@@ -45,8 +53,9 @@ export function AuthProvider({ children }) {
         signup,
         login,
         logout,
-        resetPassword
-
+        resetPassword,
+        updateEmail,
+        updatePassword
     }
 
     return (
