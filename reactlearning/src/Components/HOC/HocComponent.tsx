c2d302a6b052423329621ca381adf8cf9f4772dd
rakibsxyz <bsse0903@iit.du.ac.bd>
2021-10-11 11:38:10 +0600

    hoc

---
+++ reactlearning/src/Components/HOC/HocComponent.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ClickCouner from './ClickCouner'
+import HooverCounter from './HooverCounter'
+
+function HocComponent() {
+    return (
+        <div>
+            {/* <ClickCouner /> */}
+            <HooverCounter />
+        </div>
+    )
+}
+
+export default HocComponent
